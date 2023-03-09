7a8598e5bc3ad4d824dfa8c36d1f2c621531f1e2
nachodevV2 <126583853+nachodevV2@users.noreply.github.com>
2023-03-09 14:52:54 +0000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -14,21 +14,22 @@ local Window = Rayfield:CreateWindow({
        Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = true, -- Set this to true to use our key system
+    KeySystem = true, -- Using KeySystem
     KeySettings = {
        Title = "Infinity Alpha",
        Subtitle = "V1 Key System",
        Note = "Get the key here: https://tinyurl.com/HubKey",
-       FileName = "GandmaWareKey",
+       FileName = "IAKey",
        SaveKey = true,
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       GrabKeyFromSite = false, -- keep to false pls
        Key = "l298HOGa6pinwtFLo5dlxh2aQVpqa8gp"
     }
  })
 
 -- Home
 
- local Tab = Window:CreateTab("Home", 4483362458)
+ local Tab = Window:CreateTab("Home", 12730639613)
 
- local Section = Tab:CreateSection("⭕| Welcome")
- local Label = Tab:CreateLabel("Welcome to GandmaWare Hub!")
+ local Section = Tab:CreateSection("⭕ | Welcome")
+ local Label = Tab:CreateLabel("A quick tour of Infinity Alpha Hub")
+ local Paragraph = Tab:CreateParagraph({Title = "🔴 | Features", Content = "Infinity Alpha has a range of features that are always"})
