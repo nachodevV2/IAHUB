54c8576dc29437990cfdbe59b5b2a41658d25275
nachodevV2 <126583853+nachodevV2@users.noreply.github.com>
2023-03-10 15:45:49 +0000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Infinity Alpha",
+    Name = "Infinity Alpha v0.1.0",
     LoadingTitle = "Infinity Alpha Hub Injecting",
     LoadingSubtitle = "Loading API",
     ConfigurationSaving = {
@@ -30,7 +30,7 @@ local Window = Rayfield:CreateWindow({
 
 Rayfield:Notify({
    Title = "Warning",
-   Content = "Although we are working hard on AntiCheat bypasses, we can not ensure that you will not be banned.",
+   Content = "Although we are working hard on AntiCheat bypasses, we can not ensure that you will not be banned or detected.",
    Duration = 6.5,
    Image = 12739766654,
    Actions = { -- Notification Buttons
@@ -44,7 +44,22 @@ Rayfield:Notify({
 })
 
 -- Home
-
+ 
  local Tab = Window:CreateTab("Home", 12739766654)
 
+ -- Welcome
  local Section = Tab:CreateSection("👋 | Welcome")
+ local Paragraph = Tab:CreateParagraph({Title = "Welcome!", Content = "Welcome to Infinity Alpha Hub! This is the universal version for all games. No bypasses here, just general modules."})
+
+ -- Tutorial
+
+ local Section = Tab:CreateSection("❓| What to do")
+
+-- Update Log
+ local Section = Tab:CreateSection("ℹ️ | Updates")
+ local Paragraph = Tab:CreateParagraph({Title = "‼️ Major Update ‼️", Content = "Well, the only major update is that this is finally out!"})
+ local Paragraph = Tab:CreateParagraph({Title = "Coming soon", Content = "We are working on individual scripts for certain games. Expect this out in about 2 months!"})
+
+ -- Important Information
+
+ local Section = Tab:CreateSection("")
